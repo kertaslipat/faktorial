123c122266302cc72a932ff1bfc6d2fb15f7729f
kertaslipat <k_lipat@ymail.com>
2017-04-06 09:48:35 +0700

    Add files via upload

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -5,6 +5,8 @@
  */
 package javaapplication2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Praktikan
@@ -16,35 +18,35 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Eko Pramono (103510281)");
-System.out.println("");
-int l = 1;
-int tingkat = 10;
-int n = 12/2;
-for(int i=1;i<=n;i++){
-for(int k=n;k>i;k--){
-System.out.print(" ");
-}
-for(int j=1;j<=l;j++){
-System.out.print("+");
-}
-l+=2;
- 
-System.out.println("");
-}
-l -= 4;
- 
-for(int i=(n-1);i>=1;i--){
-for(int k=i;k<=(n-1);k++){
-System.out.print(" ");
-}
-for(int j=l;j>=1;j--){
-System.out.print("+");
-}
-l-=2;
- 
-System.out.println("");
-}
+      Scanner input = new Scanner (System.in);
+        System.out.println ("Banyaknya : ");
+        int n = input.nextInt();
+        //segitiga atas
+        for (int i=0;i<n/2+1;i++) {
+            for (int j=0;j<n/2-i;j++){
+               System.out.print(" ");
+            }
+            for (int j=0;j<2*i+1;j++){
+               if (i==0 || j==0 || j==2*i)
+                    System.out.print("*");
+               else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        //segitiga bawah
+        for (int i=n/2-1;i>=0;i--) {
+            for (int j=0;j<n/2-i;j++){
+               System.out.print(" ");
+            }
+            for (int j=0;j<2*i+1;j++){
+               if (i==n/2 || j==0 || j==2*i)
+                    System.out.print("*");
+               else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
     
 }
